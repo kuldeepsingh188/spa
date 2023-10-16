687db43d7971cb2276953b54185c97e1482588a6
kuldeepsingh18 <you@example.com>
2023-10-16 11:02:33 +0530

    first commit

---
+++ assets/js/mystyle.js
@@ -0,0 +1,83 @@
+function opennav() {
+    document.getElementById("menu").style.left = "0%";
+    document.getElementsByTagName("body")[0].style.overflow = "hidden";
+}
+function closenav() {
+    document.getElementById("menu").style.left = "-100%";
+    document.getElementsByTagName("body")[0].style.overflow = "scroll";
+}
+
+
+setTimeout(() => {
+    const loader = document.getElementById("loader");
+
+    loader.style.display = "none";
+
+    document.body.classList.remove("overflow-hidden");
+}, 3000);
+
+{
+    var btn = $('#button');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, '300');
+    });
+
+}
+
+{
+    AOS.init({
+        once: true,
+        });
+}
+{
+    $('.Rwjslider').slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        arrows: true,
+        nextArrow: ".next",
+        prevArrow: ".prev",
+        speed: 3000,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                    arrows: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: false,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false,
+                }
+            }
+        ]
+    });
+}
